37b41fc8773c2c8db4e1510d246b0b229daaf481
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-04-14 12:19:32 +0200

    Add files via upload

---
+++ nebeskoTijelo/nebeskoTijelo/NebeskoTijelo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nebeskoTijelo
+{
+    class NebeskoTijelo
+    {
+        double brzinaOkoOsi;
+        double brzinaOkoSunca;
+        public double BrzinaOkoOsi { get => brzinaOkoOsi; set => brzinaOkoOsi = value; }
+        public double BrzinaOkoSunca { get => brzinaOkoSunca; set => brzinaOkoSunca = value; }
+    }
+}
---
+++ nebeskoTijelo/nebeskoTijelo/Satelit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nebeskoTijelo
+{
+    class Satelit
+    {
+        class Satelit : NebeskoTijelo
+        {
+            public Satelit(double brzinaOkoOsi, double brzinaOkoSunca)
+            {
+                BrzinaOkoOsi = brzinaOkoOsi;
+                BrzinaOkoSunca = brzinaOkoSunca;
+            }
+        }
+    }
